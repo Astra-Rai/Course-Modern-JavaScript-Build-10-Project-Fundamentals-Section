1b71dbddbd2aa9dcfd2699fbe144ddbee7a54e9e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-12-29 09:31:28 -0600

    15-Arrays and how to create arrays

---
+++ 15-Arrays and how to create arrays .js
@@ -0,0 +1,21 @@
+//arrays are variables that can hold more than one value at a time
+
+//[] brackets, indicate an array
+
+const numbers = [10, 20, 30, 40, 50];
+//console.log(numbers);
+
+//access index of array 
+
+
+const months = new Array ('Jan', 'Feb');
+//access element in array
+//console.log(months[1]);90
+
+
+//lengh of array, months.length
+//check for array
+
+//console.log
+//check to see if array is array, it's ture
+//console.log(Array.isArray(months));
