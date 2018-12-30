b956da692612df0318b65f96c1b46ce10114ffbe
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-12-30 01:03:41 -0600

    34-Dates in JavaScript

---
+++ 34-Dates in JavaScript.js
@@ -0,0 +1,17 @@
+//current date
+const today = new Date();
+
+let output;
+ 
+//let birthday = new Date('January 5 1987');
+//output = birthday;
+
+//output = today.getMonth();
+
+//output = today.getMonth();
+//output = today.getDay();
+//output = today.getFullYear();
+//output = today.getHours();
+
+//values can also be set.
+console.log(output);
