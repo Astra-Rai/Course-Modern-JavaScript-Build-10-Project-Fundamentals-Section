9e62155571f130e98d18c65676596d9b1021e06a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-12-30 21:59:40 -0600

    3-Variable with const

---
+++ 3-Variables with const.js
@@ -0,0 +1,7 @@
+//creating variable with const
+const name = 'Astra Rai';
+console.log(name);
+//Notes about variables: const, let, var
+//with const, you always have to initiate a value
+//a const value cannot be redeclared or reassigned
+//var, can be, but it has been depreciated
