456095ea45614273dc79195591313410b201e484
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-08 10:43:41 -0600

    9-Numbers and the Methods Available Part 2

---
+++ 9-Numbers and the Methods Available Part 2.js
@@ -0,0 +1,43 @@
+//Building a video game
+let score = 5;
+score++
+score--
+
+
+//increase vaule first then print
+
+
+console.log(score);
+
+let score1 = 15;
+score1 += 10;
+console.log(score1);
+
+
+
+/*const number1 = 30,
+      number2 = 20,
+      number3 = -3;
+
+let result;
+
+//80, 30 *2 = 60, 60 + 20
+result = 20 + 30 * 2;
+
+
+//100, 50* 2
+result = (20 + 30) * 2;
+//20 % discount from a shoping cart
+const cartItems =  (20 + 30 + 30 + 40)
+const discount = (cartItems/100 *20);
+const totalPay = cartItems - discount;
+
+
+console.log('Total: $' + cartItems);
+console.log('Discount: $' + Discount);
+*/
+
+//issue?
+//console.log('To Pay: $' + totalPay);
+
+//console.log(result);
