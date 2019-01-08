a1f0fb04cd1ca4cbaea5274346d50e1159880a4a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-08 09:43:51 -0600

    8-Numbers and the Methods Available Part 1

---
+++ 8-Numbers and the Methods Available Part 1.js
@@ -0,0 +1,28 @@
+const number1 = 30,
+      number2 = 20,
+      number3 = -3;
+
+let result;
+
+//PI3.14....
+result = Math.PI;
+
+//2
+result = Math.round(2.4)
+
+
+//2
+result = Math.floor(2.7);
+
+//3
+result = Math.floor(2.7);
+
+//12
+result = Math.sqrt(144);
+
+//3
+result = Math.abs(number3)
+ 
+//512
+result = Math.pow(8, 3);
+console.log(result);
