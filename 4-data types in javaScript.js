865b9cef0fe7b81b8a96526153dc87392c17eb2c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-08 09:27:55 -0600

    .4-Data Types in JavaScript

---
+++ 4-data types in javaScript.js
@@ -0,0 +1,44 @@
+//data type exercise
+
+
+let variable = 'Juan';
+//in javascript, the value defines the datatype
+//you can reassign data in the variable
+//variable = 20;//
+//console.log(typeof variable);
+  
+//data type is a string
+//variable = '20';
+//console.log(typeof variable)
+  
+ //undefined
+ //null value treated as an object in javascript
+//
+//if there is not a value in variable it will return undefined
+//let person;
+//console.log(person);
+
+
+//typeof, returns "number"
+//let number = -9;
+//console.log(typeof number);
+//symbols will review later
+
+//returns [object Symbo] {} ??
+//variable = Symbol('My new symbol');
+//console.log(variable);
+//in javascript arrays are objects
+
+
+
+//Should return: Tue Jan 08 2019 09:20:19 GMT-0600 (Central Standard Time)
+//let today = new Date();
+//console.log(today);
+/*typeof Object
+let person3 = {
+  city: 'Okinawa',
+  food: 'amazing'
+}
+
+console.log(typeof person3);
+*/
