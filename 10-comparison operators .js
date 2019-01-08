c0514dc2c0a44c1fd596cd39c5a9f69615ad1cbc
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-08 11:04:58 -0600

    Comparison Operators

---
+++ 10-comparison operators .js
@@ -0,0 +1,42 @@
+//comparison operators exercisecharacters
+
+
+//false
+//console.log('a' >'b');
+
+//true
+//console.log('b' >'a');
+
+//false
+//console.log('Z'> 'a');
+
+//ABCD....
+//a in upper case is less than z in lower case
+
+//console.log(2  == '2');
+//use strict comparison operator
+//console.log(typeof 2);
+//console.log(typeof '2');
+//strict, true
+//console.log(2 ===2);
+// one = sign, assignment, two or more comparison
+
+
+//not equal
+//false
+//console.log (2 != 2)
+
+//true, notice the space 
+//console.log('hello' != ' hello');
+
+//compare null and undefined
+
+//null is object
+//undefined has its own data type, undefined
+
+//true
+//console.log(null == undefined);
+
+
+//false
+console.log(null === undefined);
