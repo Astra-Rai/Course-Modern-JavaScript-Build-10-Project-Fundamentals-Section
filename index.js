339359f683e1b094e3ee195e5db8bae77b06e4eb
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-13 08:28:31 -0600

    Objects and Functions w/in Objects

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-//array of objects
-//let cars is an array
-let cars = [  
- //object is at Oth index of cars [ ] array 
-    {
-       model: 'Mustang',
-       engine: 6.0 
-    },
- //object is at 1st index of the cars [ ] array 
-    {
-       model: 'Camro',
-       engine: 6.1
-    },
- //object is at the second index of the cars [] array   
-    {
-        model: 'Challenger',
-        engine: 6.1
-    }
-]
-//use a for loop
-//loop through array of obeject, cars
-//output the model type of each car in the array of objects
 
-for(let i = 0; i< cars.length; i++){
- //output the key, model, for each object listed in the array elements 
-  console.log(cars[i].model);
+//objects, have key: value pairs
+//define an object using const or let
+//curely braces indicate objects in javascript
+
+const person = {
+  firstName: 'Juan',
+  lastName: 'Del la toree',
+  job: 'Web Developer',
+  email: 'email@gmail.com',
+  age: 31,
+  favoriteMusic: ['Trace', 'Rock', 'Grunge'],
+//this s an object within an object
+  living: {
+        city: 'Guadalajara',
+        country: 'Mexico'
+  },
+ //a function is called a method when it's inside an object
+  
+  bornYear: function() {
+//new Date ()
+//new Date()is a constructor, it creates a new date object with the current date and time:      
+// new Date.getFullYear
+//  The getFullYear() method returns the year (four digits for dates between year 1000 and 9999) of the specified date
+//the keyword, this will allow us to access the value associated with
+//the key named "age"..this.age, return the same data as person.age   
+      return new Date().getFullYear() - person.age;
+    },
+//console.log(person.bornYear()); //->1988  
+  
+  //getAge: function() {
+   // return 31;
+//function name, colon space, keyword funciton()
+  //is this how to name functions within arrays
+  getJob: function (){
+    return this.job;
+  }
+
 }
-//objects are made of key: value paris
-//key:value
-//model: 'Mustang'
-//model: 'Camro'
-//model: 'Challenger
 
-//console.log(carsd[1].model);// -> "Camero"
+//console.log(person.bornYear() );
+
+//you can access the value of a key, by dot notation
+//console.log(person.living.country);// -> "Mexico"
+//an alternative to accessing values is brackets
+//make sure to wrap key name in single quotes
+//console.log(person['email']);// -> "email@gmail.com"
+
+//console.log(person.getJob());
