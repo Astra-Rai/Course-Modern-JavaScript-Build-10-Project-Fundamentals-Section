2d95bbfba965c070d913f290fb0c85326930a9bd
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-13 13:19:32 -0600

    Switch Statement

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
+//switch
 
-//objects, have key: value pairs
-//define an object using const or let
-//curely braces indicate objects in javascript
+//assigning a variable from the switch case
+//create an array, elements three cars
+const cars = ['Camero', 'Mustang', 'Challenger'];
 
-const person = {
-  firstName: 'Juan',
-  lastName: 'Del la toree',
-  job: 'Web Developer',
-  email: 'email@gmail.com',
-  age: 31,
-  favoriteMusic: ['Trace', 'Rock', 'Grunge'],
-//this s an object within an object
-  living: {
-        city: 'Guadalajara',
-        country: 'Mexico'
-  },
- //a function is called a method when it's inside an object
-  
-  bornYear: function() {
-//new Date ()
-//new Date()is a constructor, it creates a new date object with the current date and time:      
-// new Date.getFullYear
-//  The getFullYear() method returns the year (four digits for dates between year 1000 and 9999) of the specified date
-//the keyword, this will allow us to access the value associated with
-//the key named "age"..this.age, return the same data as person.age   
-      return new Date().getFullYear() - person.age;
-    },
-//console.log(person.bornYear()); //->1988  
-  
-  //getAge: function() {
-   // return 31;
-//function name, colon space, keyword funciton()
-  //is this how to name functions within arrays
-  getJob: function (){
-    return this.job;
-  }
+//use a variable that can change which case you select
+//the case you select represents on of the elments in teh array of cars
 
+const selecter = 1;
+//use this as a placeholder
+//the variable named car will hold the element of the array that is selected by the const selector variable
+let car;
+
+switch(selected){
+    
+  case 0:
+    //cars[0], camero
+    car = cars[0]; 
+    break;
+  case 1: 
+    //cars[1], mustang
+    car = cars[1];
+    break;
+    //cars[2], challenger
+  case 2:
+    car = cars[2];
+    break;
 }
 
-//console.log(person.bornYear() );
-
-//you can access the value of a key, by dot notation
-//console.log(person.living.country);// -> "Mexico"
-//an alternative to accessing values is brackets
-//make sure to wrap key name in single quotes
-//console.log(person['email']);// -> "email@gmail.com"
-
-//console.log(person.getJob());
+//print the selected car
+console.log('Your selected car is ${car}');
