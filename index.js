6f0a6dc9ec50fee57f3a3df93ad2f345069b0a80
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-01-30 06:58:36 -0600

    32-While-Do-While Loops

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 
-//OR operator
 
 
-/*let cash = 1000,
-    credit = 300,
-    cartTotals = 500,
-    available = cash + credit;
-    
-//if cartTotal is less than cash OR credit amount...
-//Or statement only needs one condition to be true
 
-if(cartTotals < cash || cartTotals< credit) {
-  console.log('You can pay with cash or credit');
+//Looping through an array with while
+
+const shoppingCart = ['Product 1', 'Product 2', 'Product 3', 'Product 4'];
+
+let i = 0;
   
-  } else if (available >= cartTotals){
-    console.log('Pay with both');
-  } else{
-    console.log('Insufficient Funds');
-  }
-  
-  */
-
-//ternary operatr = false
-
-const loggedIn = false;
-console.log (loggedIn === true ? 'The user is logged in!' : 'Not Logged In, please Log In'  );
-
-//same as
-/*
-if(loggedIn ==true) {
-  'The user is logged in'
-} else{
-'Not Logged In, please log in'
+while(i < shoppingCart.length) {
+  console.log(`Product: ${shoppingCart[i]}`);
+  i++;
 }
 
-
-
-
+//Do While Loop
+//Runs at least once, no matter the condition
+/*
+let i = 5;
+do {
+//increment the variable i by 1 as while i is less than the number 10   
+  console.log(`Number: ${i}`);
+  i++;
+} while (i < 10);
 */
